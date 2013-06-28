02d972d00d7ae592a41d07eb0dccf65daa6790e0
Ryan Faerman <ry@nwitty.com>
2013-06-27 21:35:29 -0400

    syncing works.. yea... you heard me

--- src/com/example/sqliteplaytime/DatabaseHelper.java
+++ src/com/example/sqliteplaytime/DatabaseHelper.java
@@ -7,32 +7,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-	private static final String DATABASE_NAME = "tweetDB";
-	private static final int DATABASE_VERSION = 4;
+	private static final String DATABASE_NAME = "pagesDB";
+	private static final int DATABASE_VERSION = 12;
 	private static final String TAG = "TRACE";
 	
-	public static final String CREATE_USER_TABLE = "create table if not exists users (" +
-		  "id INTEGER PRIMARY KEY,"+
-		  "name varchar(255),"+
-		  "created_at datetime default current_timestamp,"+
-		  "profile_image_url varchar(255),"+
-		  "location varchar(255),"+
-		  "followers_count int default 0,"+
-		  "verified boolean default false"+
-		");";
-	
-	public static final String CREATE_TWEET_TABLE = "create table if not exists tweets ("+
-		  "id INTEGER PRIMARY KEY,"+
-		  "user_id integer,"+
-		  "created_at datetime default current_timestamp,"+
-		  "body varchar(255),"+
-		  "retweeted boolean default false"+
-		");";
-	
-	public static final String CREATE_MENTIONS_TABLE = "create table if not exists mentions ("+
-		  "tweet_id integer,"+
-		  "user_id integer"+
-		");";
+	public static final String CREATE_TABLE = "create table if not exists pages (id integer primary key autoincrement, remote_id varchar(255), title varchar(255), body text, dirty boolean default true, updated_at integer, category varchar(255))";
+	public static final String CREATE_INDEX = "create unique index if not exists remote_id_idx ON pages(remote_id);";
 	
 	DatabaseHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -41,82 +21,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		Log.i(TAG, "creating tables");
-		db.execSQL(CREATE_USER_TABLE);
-		db.execSQL(CREATE_TWEET_TABLE);
-		db.execSQL(CREATE_MENTIONS_TABLE);
-		
-		
-		ContentValues user_values_0 = new ContentValues();
-		user_values_0.put("id", 119476949);
-		user_values_0.put("name", "OAuth Dancer");
-		user_values_0.put("created_at", "Wed Mar 03 19:37:35 +0000 2010");
-		user_values_0.put("profile_image_url", "http://a0.twimg.com/profile_images/730275945/oauth-dancer_normal.jpg");
-		user_values_0.put("location", "San Francisco CA");
-		user_values_0.put("followers_count", 28);
-		user_values_0.put("verified", false);
-		db.insert("users", null, user_values_0);
-		Log.i(TAG, "added users");
-
-		ContentValues user_values_1 = new ContentValues();
-		user_values_1.put("id", 8285392);
-		user_values_1.put("name", "Raffi Krikorian");
-		user_values_1.put("created_at", "Sun Aug 19 14:24:06 +0000 2007");
-		user_values_1.put("profile_image_url", "http://a0.twimg.com/profile_images/1270234259/raffi-headshot-casual_normal.png");
-		user_values_1.put("location", "San Francisco California");
-		user_values_1.put("followers_count", 18752);
-		user_values_1.put("verified", false);
-		db.insert("users", null, user_values_1);
-		Log.i(TAG, "added users");
-
-		ContentValues user_values_2 = new ContentValues();
-		user_values_2.put("id", 819797);
-		user_values_2.put("name", "Taylor Singletary");
-		user_values_2.put("created_at", "Wed Mar 07 22:23:19 +0000 2007");
-		user_values_2.put("profile_image_url", "http://a0.twimg.com/profile_images/2546730059/f6a8zq58mg1hn0ha8vie_normal.jpeg");
-		user_values_2.put("location", "San Francisco CA");
-		user_values_2.put("followers_count", 7126);
-		user_values_2.put("verified", "false");
-		db.insert("users", null, user_values_2);
-		Log.i(TAG, "added users");
-
-		ContentValues tweet_values_0 = new ContentValues();
-		tweet_values_0.put("id", "240558470661799936");
-		tweet_values_0.put("user_id", 119476949);
-		tweet_values_0.put("created_at", "Tue Aug 28 21:16:23 +0000 2012");
-		tweet_values_0.put("body", "just another test");
-		tweet_values_0.put("retweeted", "false");
-		db.insert("tweets", null, tweet_values_0);
-		Log.i(TAG, "added tweets");
-
-		ContentValues tweet_values_1 = new ContentValues();
-		tweet_values_1.put("id", "240556426106372096");
-		tweet_values_1.put("user_id", 8285392);
-		tweet_values_1.put("created_at", "Tue Aug 28 21:08:15 +0000 2012");
-		tweet_values_1.put("body", "lecturing at the analyzing big data with twitter class at @cal with @othman  http://t.co/bfj7zkDJ");
-		tweet_values_1.put("retweeted", "false");
-		db.insert("tweets", null, tweet_values_1);
-		Log.i(TAG, "added tweets");
-
-		ContentValues tweet_values_2 = new ContentValues();
-		tweet_values_2.put("id", "240539141056638977");
-		tweet_values_2.put("user_id", 819797);
-		tweet_values_2.put("created_at", "Tue Aug 28 19:59:34 +0000 2012");
-		tweet_values_2.put("body", "You'd be right more often if you thought you were wrong.");
-		tweet_values_2.put("retweeted", "true");
-		db.insert("tweets", null, tweet_values_2);
-		Log.i(TAG, "added tweets");
-
-		ContentValues mention_values_0 = new ContentValues();
-		mention_values_0.put("tweet_id", "240556426106372096");
-		mention_values_0.put("user_id", 119476949);
-		db.insert("mentions", null, mention_values_0);
-		Log.i(TAG, "added mentions");
-
-		ContentValues mention_values_1 = new ContentValues();
-		mention_values_1.put("tweet_id", "240556426106372096");
-		mention_values_1.put("user_id", 819797);
-		db.insert("mentions", null, mention_values_1);
-		Log.i(TAG, "added mentions");
+		db.execSQL(CREATE_TABLE);
+		db.execSQL(CREATE_INDEX);
 
 
 		Log.i(TAG, "done");
@@ -125,9 +31,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		Log.i(TAG, "dropping tables");
-		db.execSQL("drop table if exists users");
-		db.execSQL("drop table if exists tweets");
-		db.execSQL("drop table if exists mentions");
+		db.execSQL("drop table if exists pages");
 		Log.i(TAG, "drop complete");
 		
 		onCreate(db);
--- src/com/example/sqliteplaytime/MainActivity.java
+++ src/com/example/sqliteplaytime/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sqliteplaytime;
 
+import java.sql.Date;
 import java.util.ArrayList;
+
 
 import java.util.List;
 
@@ -8,17 +10,29 @@ import com.example.sqliteplaytime.R.string;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SimpleAdapter;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
 
 public class MainActivity extends Activity {
 	SQLiteOpenHelper _dbHelper;
@@ -34,6 +48,11 @@ public class MainActivity extends Activity {
 		_dbHelper = new DatabaseHelper(_context);
 		_db = _dbHelper.getWritableDatabase();
 		
+		Parse.initialize(this, "umROMo87Wx1C9sUdX3UM2TV512CemMd0m6jFOzdZ", "LSQLLijsgCeNLFDvI00UF3h6dZraiSNJmRNLp9hU");
+		ParseAnalytics.trackAppOpened(getIntent());
+		
+		sync();
+		
 		
 		Log.i("TRACE", String.valueOf(all()));
 		
@@ -44,15 +63,22 @@ public class MainActivity extends Activity {
 		tweet_list.setAdapter(arrayAdapter);
 		Log.i("TRACE", "c");
 		
+		tweet_list.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+				// TODO Auto-generated method stub
+				Log.i("TRACE", parent.getItemAtPosition(pos).toString());
+			}
+		});
+		
 		RadioGroup radioGrp = (RadioGroup)findViewById(R.id.radioGrp);
 		radioGrp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 			public void onCheckedChanged(RadioGroup arg0, int id) {
 				switch (id) {
 				case R.id.radio_retweeted:
 					Log.i("TRACE", "ID");
-					ArrayAdapter<String> arrayAdapter_retweeted = new ArrayAdapter<String>(_context,android.R.layout.simple_list_item_1, retweeted());
-					((ListView) findViewById(R.id.tweet_list)).setAdapter(arrayAdapter_retweeted);
-					break;
+					
 				case R.id.radio_all:
 					Log.i("TRACE", "ID");
 					ArrayAdapter<String> arrayAdapter_all = new ArrayAdapter<String>(_context,android.R.layout.simple_list_item_1, all());
@@ -68,33 +94,120 @@ public class MainActivity extends Activity {
 		
 	}
 	
-	public List<String> all() {
-		List<String> tweets = new ArrayList<String>();
-		String[] fields = {"body"};
-		Cursor c = _db.query("tweets", fields, null, null, null, null, null);
-		
-		if(c.getCount() > 0){
-			while(c.moveToNext()){
-				tweets.add(c.getString(c.getColumnIndex("body")));
+	public void sync() {
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Page");
+		query.findInBackground(new FindCallback<ParseObject>() {
+			
+			@Override
+			public void done(List<ParseObject> pageList, ParseException e) {
+				if(e==null) {
+					ContentValues parseValue = new ContentValues();
+					String title;
+					String body;
+					String updated_at;
+					String remote_id;
+					String query;
+					String category;
+					for(ParseObject p : pageList) {
+						parseValue.clear();
+						Log.i("TRACE", String.valueOf(p.get("title")));
+						title = "";
+						body = "";
+						updated_at = "";
+						remote_id = "";
+						category = "";
+								
+						Log.i("TRACE", "k");
+						title = String.valueOf(p.get("title"));
+						body = String.valueOf(p.get("body"));
+						updated_at = String.valueOf(p.get("updatedAt"));
+						remote_id = String.valueOf(p.getObjectId());
+						category = String.valueOf(p.get("category"));
+						
+						Log.i("TRACE", "j");
+						parseValue.put("title", String.valueOf(p.get("title")));
+						parseValue.put("body", String.valueOf(p.get("body")));
+						parseValue.put("updated_at", String.valueOf(p.get("updatedAt")));
+						parseValue.put("remote_id", String.valueOf(p.getObjectId()));
+						Log.i("TRACE", "m");
+						
+						// remote new, local missing
+						query = "insert into pages (title, body, category, updated_at, remote_id, dirty) values"
+								+ "('"+title+"','"+body+"', '"+category+"' ,'"+p.getUpdatedAt()+"','"+remote_id+"', 'false')";
+						
+						try {
+							Log.i("TRACE", query);
+							_db.execSQL(query);
+						} catch (Exception e2) {
+							// TODO: handle exception
+							
+							// remote changed, local stale
+							query = "update pages set title='"+title+"', body='"+body+"', updated_at='"+p.getUpdatedAt()+"', category='"+category+"', dirty='false' "
+									+ " where remote_id='"+remote_id+"' and updated_at < '"+p.getUpdatedAt()+"'";
+							Log.i("TRACE", query);
+							_db.execSQL(query);
+						}
+						
+						
+						// remote missing or stale, local new
+						String[] fields = {"id","title", "body", "category"};
+						final Cursor c = _db.query("pages", fields, "dirty=?", new String[] { String.valueOf(true) }, null, null, null);
+						Log.i("TRACE", "q");
+						if(c.getCount() > 0){
+							Log.i("TRACE", "w");
+							final ParseObject npo = new ParseObject("page");
+							while(c.moveToNext()){
+								Log.i("TRACE", "e");
+								npo.put("title", c.getString(c.getColumnIndex("title")));
+								npo.put("body", c.getString(c.getColumnIndex("body")));
+								npo.put("category", c.getString(c.getColumnIndex("category")));
+								npo.saveInBackground(new SaveCallback() {
+									
+									@Override
+									public void done(ParseException e) {
+										Log.i("TRACE", e.getMessage());
+										// mark locally as clean and save remote_id. ensure newest updated_at
+										String remote_id = String.valueOf(npo.getObjectId());
+										String id = c.getString(c.getColumnIndex("category"));
+										String query = "update pages set remote_id='"+remote_id+"', updated_at='"+npo.getUpdatedAt()+"', dirty='false'"
+												+ " where id='"+id+"'";
+										Log.i("TRACE", query);
+										_db.execSQL(query);
+									}
+								});
+							}
+						}
+					}
+					
+					ListView tweet_list = (ListView) findViewById(R.id.tweet_list);
+					Log.i("TRACE", "a");
+					ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(_context,android.R.layout.simple_list_item_1, all());
+					Log.i("TRACE", "B");
+					tweet_list.setAdapter(arrayAdapter);
+					Log.i("TRACE", "c");
+				} else {
+					Log.i("TRACE", "Error: " + e.getMessage());
+				}
+				
 			}
-		}
+		});
 		
-		return tweets;
 	}
 	
-	public List<String> retweeted() {
-		List<String> tweets = new ArrayList<String>();
-		String[] fields = {"body"};
-		Cursor c = _db.query("tweets", fields, "retweeted=?", new String[] { String.valueOf(true) }, null, null, null);
+	public List<String> all() {
+		List<String> pages = new ArrayList<String>();
+		String[] fields = {"title"};
+		Cursor c = _db.query("pages", fields, null, null, null, null, null);
 		
 		if(c.getCount() > 0){
 			while(c.moveToNext()){
-				tweets.add(c.getString(c.getColumnIndex("body")));
+				pages.add(c.getString(c.getColumnIndex("title")));
 			}
 		}
 		
-		return tweets;
+		return pages;
 	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
